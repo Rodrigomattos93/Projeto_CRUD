fa26cb2355638548233810430cb67792db6716e3
Rodrigo Mattos <rodrigo.mattos.rocha@poli.ufrj.br>
2025-04-16 11:24:19 -0300

    build: inclusão do models.py

---
+++ app/models.py
@@ -0,0 +1,17 @@
+from sqlalchemy import Column, Integer, String, Boolean, DateTime
+from sqlalchemy.sql import func
+from db import Base
+
+class BookModel(Base):
+    __tablename__ = "books"
+
+    id = Column(Integer, primary_key = True, index = True)
+    title = Column(String, index = True)
+    author = Column(String, index = True)
+    publication_year = Column(Integer, index =True, nullable = True)
+    genre = Column(String, index = True, nullable = True)
+    publisher = Column(String, index = True, nullable = True)
+    page_count = Column(String, index = True, nullable = True)
+    description = Column(String, index = True, nullable = True)
+    is_avaiable = Column(Boolean, index = True)
+    created_at = Column(DateTime, default = func.now(), index = True)
