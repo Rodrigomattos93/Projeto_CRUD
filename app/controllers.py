0d59ed5f959cec81ec752f828b7f1537a49caaed
Rodrigo Mattos <rodrigo.mattos.rocha@poli.ufrj.br>
2025-04-27 12:05:04 -0300

    fix: ajuste na função update do controllers.py

--- app/controllers.py
+++ app/controllers.py
@@ -38,21 +38,21 @@ def update_book(db: Session, book: BookUpdate, book_id: int):
     if db_book is None:
         return None
 
-    if db_book.author is not None:
+    if book.author is not None:
         db_book.author = book.author
-    if db_book.description is not None:
+    if book.description is not None:
         db_book.description = book.description
-    if db_book.genre is not None:
+    if book.genre is not None:
         db_book.genre = book.genre
-    if db_book.is_available is not None:
+    if book.is_available is not None:
         db_book.is_available = book.is_available
-    if db_book.page_count is not None:
+    if book.page_count is not None:
         db_book.page_count = book.page_count
-    if db_book.publisher is not None:
+    if book.publisher is not None:
         db_book.publisher = book.publisher
-    if db_book.publication_year is not None:
+    if book.publication_year is not None:
         db_book.publication_year = book.publication_year
-    if db_book.title is not None:
+    if book.title is not None:
         db_book.title = book.title
     
     db.commit()
